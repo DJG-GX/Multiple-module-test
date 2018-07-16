c2090c523c44a6f34dc4760f7d8a29ef7887e404
GXLOCAL\daang <daan.gilissen@gxsoftware.com>
2018-07-16 10:23:33 +0200

    Revert "[Child1] update App"
    This reverts commit fb7059ba77017562bb9ccf739e2faa23c17c520c.

--- Child1/src/main/java/com.gxwebmanager.solutions.Child1/App.java
+++ Child1/src/main/java/com.gxwebmanager.solutions.Child1/App.java
@@ -4,6 +4,5 @@ public class App{
 
     public static void main(String[] args) {
         System.out.println("some simple print statement");
-        System.out.println("another print statement");
     }
 }
