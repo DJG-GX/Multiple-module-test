5a5d812f13069e855c6a66cdd99acc67d44897fa
GXLOCAL\daang <daan.gilissen@gxsoftware.com>
2018-07-16 14:09:57 +0200

    update

--- Child1/src/main/java/com.gxwebmanager.solutions.Child1/App.java
+++ Child1/src/main/java/com.gxwebmanager.solutions.Child1/App.java
@@ -4,5 +4,6 @@ public class App{
 
     public static void main(String[] args) {
         System.out.println("some simple print statement");
+        System.out.println("print statement");
     }
 }
